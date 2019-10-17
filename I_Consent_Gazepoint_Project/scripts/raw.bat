49d3389f9257156c801290a4f5c26d239522a632
Jeremy Thompson <jhthompson@upei.ca>
2019-10-17 13:30:03 -0400

    Starting to fill in config data

--- I_Consent_Gazepoint_Project/scripts/raw.bat
+++ I_Consent_Gazepoint_Project/scripts/raw.bat
@@ -1,10 +1,10 @@
 set PYTHON=python
 
-set WIDTH=1024
-set HEIGHT=768
-set HERTZ=500
-set DIST=23.62
-set SCREEN=19
+set WIDTH=1680
+set HEIGHT=1050
+set HERTZ=60
+set DIST=22.44 REM 57cm
+set SCREEN=22
 
 set INDIR=../../data/tutorial_etra18/
 set IMGDIR= ../../stimulus/static/screenshots/
@@ -21,4 +21,5 @@ REM %PYTHON% tsv2raw.py --removeblinks --indir=%INDIR% --group=ctrl --outdir=%RA
 REM %PYTHON% tsv2raw.py --removeblinks --indir=%INDIR% --group=adhd --outdir=%RAWDIR% --width=%WIDTH% --height=%HEIGHT% --dist=%DIST%
 REM %PYTHON% tsv2raw.py --removeblinks --indir=%INDIR% --group=adhd_ctrl --outdir=%RAWDIR% --width=%WIDTH% --height=%HEIGHT% --dist=%DIST%
 REM %PYTHON% tsv2raw.py --removeblinks --indir=%INDIR% --group=aspd --outdir=%RAWDIR% --width=%WIDTH% --height=%HEIGHT% --dist=%DIST%
-%PYTHON% tsv2raw.py --removeblinks --indir=%INDIR% --group=pilot --outdir=%RAWDIR% --width=%WIDTH% --height=%HEIGHT% --dist=%DIST%
+REM %PYTHON% tsv2raw.py --removeblinks --indir=%INDIR% --group=pilot --outdir=%RAWDIR% --width=%WIDTH% --height=%HEIGHT% --dist=%DIST%
+%PYTHON% csv2raw.py --removeblinks --indir=%INDIR% --group=pilot --outdir=%RAWDIR% --width=%WIDTH% --height=%HEIGHT% --dist=%DIST%
